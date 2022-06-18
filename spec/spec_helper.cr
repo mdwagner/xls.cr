0c44121fc44acaa81a49c264485bc4e98eca5040
Michael Wagner <mdwagner@users.noreply.github.com>
2022-06-18 01:35:55 +0000

    feat: spectator specs

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,12 @@
-require "spec"
 require "../src/xls"
+require "spectator"
+
+module Helpers
+  def fixture_path(filename)
+    Path.new("#{Dir.current}/spec/fixtures/#{filename}")
+  end
+
+  def test_fixture
+    fixture_path("test.xls")
+  end
+end
--- src/xls.cr
+++ src/xls.cr
@@ -7,10 +7,24 @@ module Xls
       String.new(LibXls.version)
     end
 
-    def self.new(path : Path, charset : String = "UTF-8")
+    def self.open_file(path : Path, charset : String = "UTF-8")
       raise "File not found" unless File.file?(path)
       wb = LibXls.open_file(path.to_s, charset, out error)
       new(wb, error)
+    end
+
+    def self.open(file_or_content)
+      begin
+        instance = new file_or_content
+        yield instance.workbook
+      ensure
+        instance.try &.close!
+      end
+    end
+
+    def self.new(path : Path)
+      raise "File not found" unless File.file?(path)
+      new File.read(path)
     end
 
     def self.new(content : String)
